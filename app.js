c978e317c3c387694d60a3fac3fbee27be73f36e
Bishal Sharma <sharma.bishal1234@hotmail.com>
2021-07-21 10:22:25 +0545

    initial express server with routes

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const users = require('./routes/users.js')
+const posts = require('./routes/posts.js')
+const profile = require('./routes/profile.js')
+
+mongoose
+  .connect(process.env.DBURI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('Connected to Database')
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
+
+app.get('/', (req, res) => {
+  res.send('welcome home')
+})
