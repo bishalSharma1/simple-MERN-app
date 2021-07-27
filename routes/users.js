bc8c033a969ceca07d48e9c3ee32cf8c5bdc7d2f
Bishal Sharma <sharma.bishal1234@hotmail.com>
2021-07-27 10:33:45 +0545

    posts model

--- routes/users.js
+++ routes/users.js
@@ -4,17 +4,28 @@ require('dotenv').config()
 const passport = require('passport')
 const register = require('../controllers/users/register.js')
 const login = require('../controllers/users/login.js')
+const User = require('../models/User')
 
-router.route('/').get((req, res) => {
-  res.status(200).json({ message: 'works' })
+router.route('/all').get((req, res) => {
+  const errors = {}
+  User.find()
+    .then((users) => {
+      if (!users) {
+        errors.nousers = 'no users found'
+        return res.status(404).json(errors)
+      }
+      res.status(200).json(users)
+    })
+    .catch((err) => {
+      res.json(err)
+    })
 })
 
-//public route
+//public routes
 router.route('/register').post(register)
-
-//private router
 router.route('/login').post(login)
 
+//private route
 router
   .route('/current')
   .get(passport.authenticate('jwt', { session: false }), (req, res) => {
